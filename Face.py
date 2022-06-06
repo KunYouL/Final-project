9d122513ea226881a5d32362f883d0f9793e78ad
KunYou <4a8g0107@stust.edu.tw>
2022-06-06 09:19:16 +0800

    Face

--- Face.py
+++ Face.py
@@ -1,15 +1,62 @@
+import PIL.Image
+from PIL import Image, ImageTk
+import tkinter as tk
 import cv2 as cv
+from tkinter import filedialog
+from matplotlib import pyplot as plt
+import numpy as np
+import math
 
-img = cv.imread('photo/123.jpg')   #讀圖片
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)   #轉灰階
-faceCascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')   #載入辨識模型
-imgrect = faceCascade.detectMultiScale(gray, 1.1, 5)   #辨識眼  elon(2.2, 8) ppl(1.1, 5) aks(1.1, 3)
-#參數(掃得圖，每檢測完擴大繼續掃，成功檢測次數(越多次可能仔細，導致可能找不到))
-print(len(imgrect)) #看偵測幾張
 
-for(x, y, w, h) in imgrect:        #可能多部位 用迴圈跑
-    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+class Myfunction:
+    def open_file1(self):
+        filetypes = (
+            ('png file', '*.png'),
+            ('jpg file', '*.jpg'),
+            ('All file', '*.*')
+        )
+        filename = filedialog.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes
+        )
+        img = cv.imread(filename)
+        return img
 
-cv.imshow('img', img)     #顯示圖片
+    def open_file(self):
+        filetypes = (
+            ('png file', '*.png'),
+            ('jpg file', '*.jpg'),
+            ('All file', '*.*')
+        )
+        filename = filedialog.askopenfilename(
+            title='Open a file1',
+            initialdir='/',
+            filetypes=filetypes
+        )
+        img = cv.imread(filename)
+        cv.imshow('img', img)
+        cv.waitKey()
 
-cv.waitKey()
+    def save_file(self):
+        pass
+
+    def face_recognition(self):
+        src = self.open_file1()
+        gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+        faceCascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+        imgrect = faceCascade.detectMultiScale(gray, 1.1, 5)  # 辨識眼  elon(2.2, 8) ppl(1.1, 5) aks(1.1, 3)
+        # 參數(掃得圖，每檢測完擴大繼續掃，成功檢測次數(越多次可能仔細，導致可能找不到))
+        print(len(imgrect))  # 看偵測幾張
+
+        for (x, y, w, h) in imgrect:  # 可能多部位 用迴圈跑
+            cv.rectangle(src, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv.imshow('face', src)  # 顯示圖片
+
+    def concatenate(self):
+        src1 = self.open_file1()
+        src2 = self.open_file1()
+
+        image = np.concatenate((src1, src2))
+        cv.imshow('image', image)
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import ImageTk, Image
 import cv2 as cv
 from tkinter import filedialog
 from matplotlib import pyplot as plt
-import Face1
+import Face
 
 class App:
     def __init__(self, window, window_title):
@@ -13,7 +13,7 @@ class App:
         self.window.title('My window')
         self.window.geometry('1000x600')
 
-        self.my_function = Face1.Myfunction()
+        self.my_function = Face.Myfunction()
 
         # Add a menubar
         self.main_menu = tk.Menu(window)  # 創建視窗
@@ -28,15 +28,11 @@ class App:
         # Add operation1 submenu
         self.operation1_menu = tk.Menu(self.main_menu, tearoff=0)
         self.operation1_menu.add_command(label='人臉辨識', command=self.my_function.face_recognition)
-
-        # Add operation2 submenu
-        self.operation2_menu = tk.Menu(self.main_menu, tearoff=0)
-        self.operation2_menu.add_command(label='小畫家', command=self.my_function.Painter)
+        self.operation1_menu.add_command(label='圖像合併', command=self.my_function.concatenate)
 
         # Add  submenu to mainmenu
         self.main_menu.add_cascade(label="檔案", menu=self.file_menu)
         self.main_menu.add_cascade(label='功能1', menu=self.operation1_menu)
-        self.main_menu.add_cascade(label='功能2', menu=self.operation2_menu)
 
         # display menu
         self.window.config(menu=self.main_menu)
